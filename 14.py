e11b53cd2eb727433ea93d0080d03f0ff795ed9e
bakhritdinov99 <fbaxritdinov0099@gmail.com>
2022-03-25 09:44:36 -0700

    14-masala

---
+++ 14.py
@@ -0,0 +1,18 @@
+N,K=map(int,input().split())
+
+def pow_mod(x, y, z):
+    number = 1
+    while y:
+        if y & 1:
+            number = number * x % z
+        y >>= 1
+        x = x * x % z
+    return number
+b=10**9+7
+x=pow_mod(K+1,N,b)
+
+if N==0:
+    print(1)
+else:
+    print(x)
+
