265f1b131a2bc066ce6511cd313741a4aa344789
bakhritdinov99 <fbaxritdinov0099@gmail.com>
2022-03-24 23:16:18 -0700

    12-masala

---
+++ 12.py
@@ -0,0 +1,21 @@
+n=int(input())
+sanoq=0
+def eratosthenes(n):     # n - число, до которого хотим найти простые числа
+    sieve = list(range(n + 1))
+    sieve[1] = 0    # без этой строки итоговый список будет содержать единицу
+    for i in sieve:
+        if i > 1:
+            for j in range(2*i, len(sieve), i):
+                sieve[j] = 0
+
+    return sieve
+a=eratosthenes(n)
+
+tub = []
+for x in a:
+    if x!=0:
+        tub.append(x)
+if len(tub)%2==0:
+    print('Bobur')
+else:
+    print('Ali')
