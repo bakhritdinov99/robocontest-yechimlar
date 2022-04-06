ff23db904d161351b09b1bb0dcc3d4011926e5db
bakhritdinov99 <fbaxritdinov0099@gmail.com>
2022-04-06 16:04:04 +0500

    TL12

--- 17.py
+++ 17.py
@@ -1,31 +1,18 @@
-n = int(input())
-
-l = []
-g=[]
-
-i=1
-q=n*1000
-while i<q:
-    l.append(i)
-    i+=1
-
-def raq_yig(r):
+def summ(n):
     s=0
-    while r:
-        s+=r%10
-        r//=10
+    while n>0:
+        s+=n%10
+        n//=10
     return s
-
-j=1
-while j<q:
-    if j%pow(raq_yig(j),2)==0:
-        g.append(j)
-    j+=1
-if n==1:
-    print(1)
-elif n==2:
-    print(10)
-elif n==3:
-    print(20)
-else:
-    print(g[n-1])
+def check(n):
+    s=summ(int(n))
+    return n%(s*s)==0
+def get(n):
+    r,x=0,0
+    while r<n:
+        x+=1
+        if check(x):
+            r+=1
+    return x
+n=input()
+print(get(int(n)))
